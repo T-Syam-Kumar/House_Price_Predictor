85c54eb6d3124160b470381638c2148127a56d7a
Syam Kumar Teku <tsyamkumar143@gmail.com>
2026-05-28 09:36:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-# Absolute path handling
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
 DATA_PATH = os.path.join(BASE_DIR, "house_data.csv")
 
-# Debug: show files (remove later)
+
 st.write("Files in directory:", os.listdir(BASE_DIR))
 
-# Load model safely
+
 if not os.path.exists(MODEL_PATH):
     st.error("❌ model.pkl not found. Please upload it to the GitHub repo.")
     st.stop()
